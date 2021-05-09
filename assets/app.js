871df407ae94852870e466d373edbdb0f7227ba3
Diego Bonilla <drbv27@gmail.com>
2021-05-09 13:54:07 -0500

    Initial Commit

---
+++ assets/app.js
@@ -0,0 +1,34 @@
+/*Ejercicios Basicos JS Nivel 1
+    5.Programa una función que invierta las palabras de una cadena de texto.Ejemplo: miFuncion("Hola Mundo") devolverá "odnuM aloH".
+    6.Programa una función para contar el número de veces que se repite una palabra en un texto largo.Ejemplo: miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+    7.Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro).Ejemplo mifuncion("Salas") devolverá true.
+    8.Programa una función que elimine cierto patrón de caracteres de un texto dado.Ejemplo: miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+*/
+
+//Soluciones:
+
+//--------------------------------------------------------------------------------------------
+
+//E5S1.Solucion Simple:
+console.log("Ejercicio 5:")
+console.log("-----------------------------------------------------------------------------------")
+
+
+//--------------------------------------------------------------------------------------------
+
+//E6S1.Solución Simple
+console.log("Ejercicio 6:")
+console.log("-----------------------------------------------------------------------------------")
+
+
+//--------------------------------------------------------------------------------------------
+
+//E7S1.Solución Simple
+console.log("Ejercicio 7:")
+console.log("-----------------------------------------------------------------------------------")
+
+//--------------------------------------------------------------------------------------------
+
+//E8S1.Solucion Simple
+console.log("Ejercicio 8:")
+console.log("-----------------------------------------------------------------------------------")
